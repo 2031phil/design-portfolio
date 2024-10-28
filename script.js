7d4e0932f9c4ec73376a76f6c6213b9f6327c660
Philip Horlemann <phil@2031.local>
2024-10-28 11:36:54 +0100

    added hospital app project

--- script.js
+++ script.js
@@ -1,11 +1,120 @@
-let projectsGrid=document.getElementsByClassName("projects-grid")[0];let titleText=document.getElementsByClassName("title-text")[0];let monogram=document.getElementById("monogram");let pageLink=document.getElementById("notSelected");let currentPage=document.getElementById("selected");let contentContainer=document.getElementsByClassName("content-container")[0];let projectCard=document.querySelectorAll(".project-card");let projectBackdrop=document.getElementsByClassName("project-backdrop")[0];let main=document.getElementsByTagName("main")[0];function scrollFader(){if(projectsGrid){projectsGrid.removeEventListener("scroll",scrollHandler)}
-if(projectsGrid&&window.innerWidth>1250){projectsGrid.addEventListener("scroll",scrollHandler)}
-if(contentContainer){contentContainer.removeEventListener("scroll",scrollHandler2)}
-if(contentContainer&&window.innerWidth>1250){contentContainer.addEventListener("scroll",scrollHandler2)}}
-function scrollHandler(){titleText.style.opacity=Math.max(0,Math.min(1,1-(projectsGrid.scrollTop/150)))}
-function scrollHandler2(){titleText.style.opacity=Math.max(0,Math.min(1,1-(contentContainer.scrollTop/80)))}
-window.addEventListener("resize",function(){scrollFader()});scrollFader();monogram.addEventListener("mouseover",function(){if(window.innerWidth>700){monogram.classList.add("animate__tada")}});monogram.addEventListener("animationend",function(){monogram.classList.remove("animate__tada")});monogram.addEventListener("click",function(){if(window.innerWidth<700){monogram.classList.add("animate__tada")}});function shortDelay(URL){setTimeout(function(){window.location=URL},500)}
-function longDelay(URL){setTimeout(function(){window.location=URL},1000)}
-pageLink.addEventListener("click",function(){if(projectsGrid){projectsGrid.classList.add("animate__fadeOut");titleText.style.transition=".5s";titleText.style.opacity="1"}
-if(contentContainer){contentContainer.classList.add("animate__fadeOut")}
-currentPage.style.opacity=".4";pageLink.style.opacity="1"});projectCard.forEach((i)=>{i.addEventListener("click",function(){if(window.innerWidth>1250&&window.innerHeight>600){projectBackdrop.style.display="block";setTimeout(function(){projectBackdrop.style.transform="translateY(0)"},1)}else{main.classList.add("animate__fadeOut");titleText.style.transition=".5s";titleText.style.opacity="0"}})});window.addEventListener("DOMContentLoaded",()=>{if(window.innerWidth<700){setTimeout(function(){pageLink.style.transition=".4s";currentPage.style.transition=".5s";projectCard.forEach((i)=>{i.style.transition=".5s"})},50)}})
+let projectsGrid = document.getElementsByClassName("projects-grid")[0];
+let titleText = document.getElementsByClassName("title-text")[0];
+let monogram = document.getElementById("monogram");
+let pageLink = document.getElementById("notSelected");
+let currentPage = document.getElementById("selected");
+let contentContainer = document.getElementsByClassName("content-container")[0];
+let projectCard = document.querySelectorAll(".project-card");
+let projectBackdrop = document.getElementsByClassName("project-backdrop")[0];
+let main = document.getElementsByTagName("main")[0];
+let body = document.getElementsByTagName("body")[0];
+
+// Fadeout of Title on Scroll
+function scrollFader() {
+    if (projectsGrid) {
+        projectsGrid.removeEventListener("scroll", scrollHandler);
+    }
+    if (projectsGrid && window.innerWidth > 1250) {
+        projectsGrid.addEventListener("scroll", scrollHandler);
+    }
+
+    if (contentContainer) {
+        contentContainer.removeEventListener("scroll", scrollHandler2);
+    }
+    if (contentContainer && window.innerWidth > 1250) {
+        contentContainer.addEventListener("scroll", scrollHandler2);
+    }
+}
+
+function scrollHandler() {
+    titleText.style.opacity = Math.max(0, Math.min(1, 1 - (projectsGrid.scrollTop / 150)));
+}
+
+function scrollHandler2() {
+    titleText.style.opacity = Math.max(0, Math.min(1, 1 - (contentContainer.scrollTop / 80)));
+}
+
+window.addEventListener("resize", function () {
+    scrollFader();
+});
+
+scrollFader();
+
+
+// Monogram Animation
+monogram.addEventListener("mouseover", function () {
+    if (window.innerWidth > 700) {
+        monogram.classList.add("animate__tada");
+    }
+});
+
+monogram.addEventListener("animationend", function () {
+    monogram.classList.remove("animate__tada");
+});
+
+monogram.addEventListener("click", function () {
+    if (window.innerWidth < 700) {
+        monogram.classList.add("animate__tada");
+    }
+});
+
+
+// URL Delay
+function shortDelay(URL) {
+    setTimeout(function () { window.location = URL }, 500);
+}
+
+function longDelay(URL) {
+    setTimeout(function () { window.location = URL }, 1000);
+}
+
+
+// Animation when clicking a Link
+pageLink.addEventListener("click", function () {
+    if (projectsGrid) {
+        projectsGrid.classList.add("animate__fadeOut");
+        titleText.style.transition = ".5s";
+        titleText.style.opacity = "1";
+    }
+    if (contentContainer) {
+        contentContainer.classList.add("animate__fadeOut");
+    }
+    currentPage.style.opacity = ".4";
+    pageLink.style.opacity = "1";
+});
+
+const projectCardArray = Array.from(projectCard);
+
+const projectCardFiltered = projectCardArray.filter((i) => {
+    return i.id !== 'photoPortfolio';
+})
+
+projectCardFiltered.forEach((i) => {
+    i.addEventListener("click", function () {
+        if (window.innerWidth > 1250 && window.innerHeight > 600) {
+            projectBackdrop.style.display = "block";
+            setTimeout(function () {
+                body.style.overflowY = "hidden";
+                projectBackdrop.style.transform = "translateY(0)";
+                main.classList.add("animate__fadeOut");
+            }, 1);
+        } else {
+            main.classList.add("animate__fadeOut");
+            titleText.style.transition = ".5s";
+            titleText.style.opacity = "0";
+        }
+    });
+});
+
+// Adding transition after DOM is loaded for Mobile Devices
+window.addEventListener("DOMContentLoaded", () => {
+    if (window.innerWidth < 700) {
+        setTimeout(function () {
+            pageLink.style.transition = ".4s";
+            currentPage.style.transition = ".5s";
+            projectCard.forEach((i) => {
+                i.style.transition = ".5s";
+            });
+        }, 50);
+    }
+});
